7596e253402c298af34313fb142a7ffd2b609857
a1225220 <kannan.murali@bestbuy.com>
2016-09-26 09:38:29 -0500

    adding fcm

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    if (request.host == 'localhost')
+        response.redirect('http://localhost:5000/index.html');
+    else
+        response.redirect('https://fcm-sample.herokuapp.com/index.html');
 })
 
 app.listen(app.get('port'), function() {
